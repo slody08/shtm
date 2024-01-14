e4153a2ed5a29967c2326951b349b6f8c249c8c2
slody08 <slody08.mail@gmail.com>
2024-01-14 15:29:07 +0200

    Update 0.3.1

---
+++ build/CMakeFiles/shtm.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/shtm.dir/src/error.cpp.o"
+  "CMakeFiles/shtm.dir/src/error.cpp.o.d"
+  "CMakeFiles/shtm.dir/src/main.cpp.o"
+  "CMakeFiles/shtm.dir/src/main.cpp.o.d"
+  "CMakeFiles/shtm.dir/src/rc.cpp.o"
+  "CMakeFiles/shtm.dir/src/rc.cpp.o.d"
+  "CMakeFiles/shtm.dir/src/shtm.cpp.o"
+  "CMakeFiles/shtm.dir/src/shtm.cpp.o.d"
+  "CMakeFiles/shtm.dir/src/task.cpp.o"
+  "CMakeFiles/shtm.dir/src/task.cpp.o.d"
+  "shtm"
+  "shtm.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/shtm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/shtm.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for shtm.
+# This may be replaced when dependencies are built.
